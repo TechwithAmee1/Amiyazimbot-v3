3663c9e54d7e2ccd7c06ecca99c4b771f70522bd
TechwithAmee1 <107279170+TechwithAmee1@users.noreply.github.com>
2022-06-27 22:39:14 +0530

    Update config.js
    Signed-off-by: TechwithAmee1 <107279170+TechwithAmee1@users.noreply.github.com>

--- config.js
+++ config.js
@@ -37,10 +37,10 @@ global.APIKeys = {
 }
 
 // Other
-global.owner = ['27634090203','']
-global.premium = ['27634090203','918188019676']
-global.botname = 'ZIM BOT INC'
-global.ownername = 'DRIPS'
+global.owner = ['15083650964','12012213168']
+global.premium = ['15083650964','12012213168']
+global.botname = 'AMIYA ZIM BOT INC'
+global.ownername = 'AMIYA'
 global.group1 = 'https://chat.whatsapp.com/EFsb8RCXV4jLEFk4eAcA1A'
 global.group2 = 'https://chat.whatsapp.com/HSfcYU13g5C8GxINWwSWoA'
 global.packname = 'ZIM BOT INC'
